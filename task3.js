3ced8703ed274e7cc6e3afbba9e84c350ddabf76
Mikita <loskytov2000@gmail.com>
2025-09-27 20:28:44 +0300

    first commit

--- task3.js
+++ task3.js
@@ -3,29 +3,32 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
-// Функция НОД
+// Функция НОД (на BigInt)
 function gcd(a, b) {
-    return b === 0 ? a : gcd(b, a % b);
+  return b === 0n ? a : gcd(b, a % b);
 }
 
-// Функция НОК
+// Функция НОК (на BigInt)
 function lcm(a, b) {
-    return (a * b) / gcd(a, b);
+  return (a * b) / gcd(a, b);
 }
 
 app.get("/loskytov2000_gmail_com", (req, res) => {
-    const { x, y } = req.query;
+  try {
+    const a = BigInt(req.query.x);
+    const b = BigInt(req.query.y);
 
-    const a = Number(x);
-    const b = Number(y);
-
-    if (!Number.isInteger(a) || a <= 0 || !Number.isInteger(b) || b <= 0) {
-        return res.send("NaN");
+    if (a <= 0n || b <= 0n) {
+      return res.send("NaN");
     }
 
     res.send(lcm(a, b).toString());
+  } catch {
+    // если не удалось преобразовать в BigInt
+    res.send("NaN");
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
